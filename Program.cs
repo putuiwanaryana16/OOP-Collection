3f1350259c8dae913f4c757eed4ec8684f30fc8f
putuiwanaryana16 <61940760+putuiwanaryana16@users.noreply.github.com>
2020-06-23 09:19:40 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasPolyDanCol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KaryawanTetap karyawanTetap = new KaryawanTetap();
+            karyawanTetap.Nik = "123-456-789";
+            karyawanTetap.Nama = "eni";
+            karyawanTetap.GajiBulanan = 5000000;
+
+            KaryawanHarian karyawanHarian = new KaryawanHarian();
+            karyawanHarian.Nik = "123-234-345";
+            karyawanHarian.Nama = "sefira";
+            karyawanHarian.JumlahJamKerja = 8;
+            karyawanHarian.UpahPerJam = 15000;
+
+            Sales sales = new Sales();
+            sales.Nik = "123-333-222";
+            sales.Nama = "wayan";
+            sales.JumlahPenjualan = 40;
+            sales.Komisi = 50000;
+
+            List<Karyawan> listKaryawan = new List<Karyawan>();
+
+            listKaryawan.Add(karyawanTetap);
+            listKaryawan.Add(karyawanHarian);
+            listKaryawan.Add(sales);
+
+            int noUrut = 1;
+
+            foreach (Karyawan karyawan in listKaryawan)
+            {
+                Console.WriteLine("{0}. Nik: {1}, Nama: {2}, Gaji: {3:N0}",
+                noUrut, karyawan.Nik, karyawan.Nama, karyawan.Gaji());
+
+                noUrut++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
